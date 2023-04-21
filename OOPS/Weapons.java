6c506e0137145c5d77538d9701413e0acbcad25f
nk <nandinikapoor458@gmail.com>
2023-04-21 11:53:03 +0530

    OOPS

---
+++ OOPS/Weapons.java
@@ -0,0 +1,20 @@
+package OOPS;
+
+class Gun
+{
+    String gunName;
+    float calibre;
+    int ammo;
+    int damage;
+
+    public Gun(String gunName,float calibre,int ammo,int damage)
+    {
+        // to access data memeber of class use this keyword to 
+        // avoid confusion btwn class data members nd fn arguments
+        this.gunName=
+    }
+}
+
+public class Weapons {
+
+}
